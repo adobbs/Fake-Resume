a350195e02a93b8a00ac0828611020976f31c3f9
Andy Dobbs <andrew.dobbs@gmail.com>
2024-01-03 19:49:18 -0500

    Initial Commit

---
+++ Fake Resume/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fake Resume
+//
+//  Created by Andy Dobbs on 1/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fake Resume/Fake_ResumeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fake_ResumeApp.swift
+//  Fake Resume
+//
+//  Created by Andy Dobbs on 1/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Fake_ResumeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
